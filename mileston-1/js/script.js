9168b9e7d33be06d36fa87fef76809f1c5be9648
bodascaluca <bodasca.luca@gmail.com>
2022-04-28 15:12:03 +0200

    Milleston-1

--- mileston-1/js/script.js
+++ mileston-1/js/script.js
@@ -1,1 +1,43 @@
-alert(`Salve`);
+
+// Viene fornito un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+// MILESTONE 1:
+// stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+  for (let i = 0; i < team.length; i++){
+    const currentInformation = team[i];
+    console.log(currentInformation.name, currentInformation.role, currentInformation.image)
+  }
--- mileston-2/js/script.js
+++ mileston-2/js/script.js
@@ -1,1 +1,6 @@
 alert(`Salve`);
+// Viene fornito un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+// MILESTONE 2:
+// stampare le stesse informazioni su DOM sottoforma di stringhe
+
